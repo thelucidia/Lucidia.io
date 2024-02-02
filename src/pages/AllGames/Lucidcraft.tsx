c5645527e699d589e8277caee49b94b2c74eabf6
nightfury1111111111 <linjay0208@gmail.com>
2024-02-02 12:14:10 +0900

    add video to zombie page

--- src/pages/AllGames/Lucidcraft.tsx
+++ src/pages/AllGames/Lucidcraft.tsx
@@ -11,53 +11,65 @@ const Lucidcraft = () => {
   const { t } = useTranslation();
 
   return (
-    <motion.section initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 0.7}} className="w-full min-h-screen bg-bg">
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.7 }}
+      className="w-full min-h-screen bg-bg"
+    >
       <div className="w-full min-h-[40vh] md:min-h-[60vh] lg:min-h-screen px-8 sm:px-auto p-5 font-primary relative text-secondary flex  justify-center bg-bg">
         <Nav />
         <img
-          src={"/lucidcraft.png"}
+          src={"/games/lucidcraft/banner.png"}
           className="w-full h-full object-cover absolute top-0 left-0 object-top"
         />
-        <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-b from-transparent via-transparent to-black/80 z-10"></div>
-
+        {/* <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-b from-transparent via-transparent to-black/80 z-10"></div> */}
+        <img
+          src={"/games/lucidcraft/title.png"}
+          className="w-[40%] object-cover absolute top-[45%] left-[30%] object-top"
+        />
         <div className="max-w-xs sm:max-w-full w-full flex flex-col justify-end gap-y-8 sm:gap-y-8 md:translate-y-0 sm:translate-y-24 translate-y-24 md:gap-y-12 md:pb-10 items-center h-auto mx-auto  z-10 mt-[4.5rem] sm:mt-[7rem] md:mt-[10rem] absolute bottom-5">
+          <p className="text-3xl text-center left-0 right-0">
+            {t("coming_soon")}
+          </p>
+        </div>
+        {/* <div className="max-w-xs sm:max-w-full w-full flex flex-col justify-end gap-y-8 sm:gap-y-8 md:translate-y-0 sm:translate-y-24 translate-y-24 md:gap-y-12 md:pb-10 items-center h-auto mx-auto  z-10 mt-[4.5rem] sm:mt-[7rem] md:mt-[10rem] absolute bottom-5">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-[96px] text-center font-bold left-0 right-0">
-          {t("lucid")}
+            {t("lucid")}
           </h1>
 
           <Button className="w-full md:w-56" stretchOnMobile>
-          {t("games.play_game")}
+            {t("games.play_game")}
           </Button>
-        </div>
+        </div> */}
       </div>
 
-      <div className="w-full h-auto px-8 py-10 -mb-20 md:mb-0">
+      <div className="w-full h-auto px-8 py-10 -mb-20 md:mb-0 bg-[url('/games/lucidcraft/grid_background.png')]">
         <div className="max-w-[1100px] pb-14 sm:pb-20 w-full min-h-screen mx-auto text-secondary font-primary justify-between flex gap-10 pt-16 md:flex-row flex-col-reverse">
           <div className="w-full md:space-y-7 space-y-4">
             <h1 className="text-xl md:text-6xl text-primary font-bold">
-            {t("games.lucid.title")}
+              {t("games.lucid.title")}
             </h1>
-            <p className=" text-base md:text-xl">
-            {t("games.lucid.desc")}
-            </p>
+            <p className=" text-base md:text-xl">{t("games.lucid.desc")}</p>
             <h2 className="text-primary md:text-2xl text-xl font-bold">
-            {t("overview")} :
+              {t("overview")} :
             </h2>
             <div className="text-base md:text-xl">
-              <p className="">
-              {t("games.lucid.overview")}
-              </p>
+              <p className="">{t("games.lucid.overview")}</p>
               <p className="">
                 <b>{t("games.lucid.mech")}:</b> {t("games.lucid.overview2")}
               </p>
             </div>
 
             <h2 className="md:text-2xl text-xl text-primary font-bold">
-            {t("game_details")} :
+              {t("game_details")} :
             </h2>
 
             <div className=" text-base md:text-xl">
-              {(t("games.lucid.details", {returnObjects: true}) as details[]).map((items, i) => (
+              {(
+                t("games.lucid.details", { returnObjects: true }) as details[]
+              ).map((items, i) => (
                 <div className="flex gap-1" key={i}>
                   <h1 className="">
                     <b>{items.title}:</b> <span>{items.value}</span>
@@ -67,7 +79,9 @@ const Lucidcraft = () => {
             </div>
 
             <div className=" text-base md:text-xl">
-              {(t("games.zombie.details2", {returnObjects: true}) as string[]).map((items, i) => (
+              {(
+                t("games.zombie.details2", { returnObjects: true }) as string[]
+              ).map((items, i) => (
                 <p className="" key={i}>
                   {items}
                 </p>
--- src/pages/AllGames/Zombie.tsx
+++ src/pages/AllGames/Zombie.tsx
@@ -19,11 +19,24 @@ const Zombie = () => {
     >
       <div className="w-full min-h-[40vh] md:min-h-[60vh] lg:min-h-screen px-8 sm:px-auto p-5 font-primary relative text-secondary flex  justify-center bg-bg">
         <Nav />
+        <div className="bg-video">
+          <video
+            className="w-full h-full object-cover absolute bottom-0 left-0 object-bottom"
+            autoPlay
+            muted
+            loop
+          >
+            <source src="/video/zombie.mp4" type="video/mp4" />
+          </video>
+        </div>
         <img
-          src={"/games/zombie_banner.png"}
-          className="w-full h-full object-cover absolute top-0 left-0 object-top"
+          src={"/games/zombie/title.png"}
+          className="h-[25%] object-cover absolute top-[30%] left-[12.5%] object-top z-30"
         />
-        <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-l from-transparent via-transparent to-black/80 z-10"></div>
+        <div className="absolute top-[55%] left-[12.5%] z-30 text-xl">
+          is coming soon. Denend yourself
+        </div>
+        <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-l from-transparent via-transparent to-black/80 z-10" />
 
         {/* <div className="max-w-xs sm:max-w-full w-full flex flex-col justify-end gap-y-8 sm:gap-y-8 md:translate-y-0 sm:translate-y-24 translate-y-24 md:gap-y-12 md:pb-10 items-center h-auto mx-auto z-10 mt-[4.5rem] sm:mt-[7rem] md:mt-[10rem] absolute bottom-5 ">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-[96px] text-center font-bold left-0 right-0">
@@ -40,10 +53,12 @@ const Zombie = () => {
         <div className="max-w-[1100px] pb-14 sm:pb-20 w-full min-h-screen mx-auto text-secondary font-primary justify-between flex gap-10 pt-16 md:flex-row flex-col-reverse">
           <div className="w-full md:space-y-7 space-y-4">
             <div className="border-primary border-[1px]">
-              <h1 className="text-xl md:text-6xl text-primary font-bold">
+              <h1 className="text-xl md:text-5xl text-primary font-bold text-center p-4">
                 {t("games.zombie.title")}
               </h1>
-              <p className=" text-base md:text-xl">{t("games.zombie.desc")}</p>
+              <p className="mt-2 p-4 bg-primary text-black text-base md:text-xl">
+                {t("games.zombie.desc")}
+              </p>
             </div>
             <h2 className="text-primary md:text-2xl text-xl font-bold">
               {t("overview")} :
