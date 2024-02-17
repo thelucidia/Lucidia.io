7b35383be238d80937bf79d15ee76c546b18820b
progspace1204 <progspace1204@gmail.com>
2024-02-17 21:04:36 +0200

    feat: modify games page's content

--- src/views/Games/GamesContent.tsx
+++ src/views/Games/GamesContent.tsx
@@ -1,85 +1,81 @@
-import { useNavigate } from 'react-router-dom';
-import Button from '../../components/Button';
-import SubtleButton from '../../components/SubtleButton';
-import { cn } from '../../utils';
 import { useTranslation } from 'react-i18next';
-
-interface gamesInterface {
-  title: string;
-  subHeader: string;
-  description: string;
-  slug: string;
-  link: string;
-  image: string;
-}
+import Button from '../../components/Button';
 
 const GamesContent = () => {
   const { t } = useTranslation();
-  console.log(t('games_content', { returnObjects: true }));
-
-  const navigate = useNavigate();
 
   return (
-    <section className="w-full min-h-screen relative px-8 lg:pt-5 pt-2 text-secondary font-primary">
-      <div className="max-w-[1100px] w-full mx-auto flex flex-col md:gap-y-24 gap-y-10">
-        {(t('games_content', { returnObjects: true }) as gamesInterface[]).map((item: any, i: number) => (
-          <div
-            className={cn('flex lg:flex-row flex-col-reverse justify-between gap-x-5', {
-              'lg:flex-row-reverse flex-col-reverse': i % 2 !== 0,
-            })}
-            key={item.title}
-          >
-            <div
-              className={cn('max-w-xl w-full md:pt-20 pt-4 md:pr-10', {
-                'mr-auto': i % 2 !== 0,
-                'md:!pt-10': i === 0,
-              })}
-            >
-              <h1 className="text-2xl md:text-4xl font-bold text-primary capitalize max-w-xl">{item.subHeader}</h1>
-
-              <p className="md:mt-10 mt-2 md:text-xl text-base break-words">{item.description}</p>
-              <div className="flex sm:gap-4 sm:flex-row flex-col mt-5 ">
-                <Button className="md:w-[13rem] w-full" stretchOnMobile>
-                  {t('games.play_game')}
-                </Button>
-                <SubtleButton
-                  className="md:w-[13rem] w-full"
-                  stretchOnMobile
-                  onClick={() => {
-                    navigate(`/games/${item.slug}`);
-                  }}
-                >
-                  {t('details')}
-                </SubtleButton>
+    <section className="w-full mt-20 text-[#F0F0F0]">
+      <div className="max-w-[1440px] w-full mx-auto">
+        <div
+          className="relative h-[384px]"
+          style={{ background: '#1E1E1E url("/games/car_bg.png") no-repeat left top' }}
+        >
+          <img src="/games/car.png" width={261} height={165} className="absolute left-[113px] top-[142px]" />
+          <div className="w-[732px] ml-[470px] pt-[72px] flex flex-col">
+            <h3 className="font-bold text-5xl leading-[58px]">{t('games_page.section1_title')}</h3>
+            <span className="font-medium text-xl leading-6 mt-8">{t('games_page.section1_description')}</span>
+          </div>
+        </div>
+        <div
+          className="h-[644px] flex flex-row justify-center align-middle items-center"
+          style={{ backgroundImage: 'url("/games/benefit_bg.png")' }}
+        >
+          <div className="flex flex-col text-right pt-4 pb-12 pr-16 border-r-2 border-[#F0F0F0] mr-24">
+            <h3 className="font-bold text-5xl leading-[58px]">{t('games_page.section2_title1')}</h3>
+            <h3 className="font-bold text-5xl leading-[58px]">{t('games_page.section2_title2')}</h3>
+            <h3 className="font-bold text-5xl leading-[58px]">{t('games_page.section2_title3')}</h3>
+          </div>
+          <ol className="font-medium text-xl leading-6 flex flex-col gap-y-12">
+            {(t('games_page.section2_description', { returnObjects: true }) as string[]).map(
+              (item: string, i: number) => (
+                <li key={i} className="w-[600px]">
+                  {item}
+                </li>
+              ),
+            )}
+          </ol>
+        </div>
+        <div className="flex flex-row justify-center align-middle items-center">
+          <div className="flex flex-col gap-y-5">
+            <div className="font-bold text-5xl mb-[72px] mt-[101px]">{t('games_page.join_us')}</div>
+            <div className="flex flex-col gap-y-5 border-r-2 pr-24 border-[#F0F0F0] mr-[104px]">
+              <div className="w-[496px] h-[52px]">
+                <input
+                  className="bg-transparent border-2 border-[#F0F0F0] w-full p-2 text-xl"
+                  type="text"
+                  placeholder={t('games_page.studio_name')}
+                />
               </div>
-            </div>
-
-            <div className="relative h-auto overflow-hidden  flex flex-col mr-auto space-y-5 sm:space-y-10 w-full sm:w-4/5">
-              <h1
-                className={cn('capitalize font-bold text-2xl sm:text-3xl md:text-5xl', {
-                  'lg:text-right text-left': i % 2 === 0,
-                })}
-              >
-                {item.title}
-              </h1>
-              <img
-                src={`/games/${item.image}.webp`}
-                alt={item.title}
-                className="object-cover w-full h-full object-bottom lg:block hidden"
-              />
-              <img
-                src={`/games/split/${item.image}.png`}
-                alt={item.title}
-                className="object-cover w-full h-full object-bottom lg:hidden block"
-              />
+              <div className="w-[496px] flex flex-row gap-x-5 h-[52px]">
+                <input
+                  className="flex-auto w-4 bg-transparent border-2 border-[#F0F0F0] p-2 text-xl"
+                  type="text"
+                  placeholder={t('games_page.email')}
+                />
+                <input
+                  className="flex-auto w-4 bg-transparent border-2 border-[#F0F0F0] p-2 text-xl"
+                  type="text"
+                  placeholder={t('games_page.website')}
+                />
+              </div>
+              <div className="w-[496px]">
+                <textarea
+                  className="h-[258px] mb-[60px] resize-none bg-transparent border-2 border-[#F0F0F0] w-full p-2 text-xl"
+                  placeholder={t('games_page.game_description')}
+                />
+              </div>
+              <Button className="w-full md:w-56" stretchOnMobile>
+                {t('games.play_game')}
+              </Button>
             </div>
           </div>
-        ))}
+          <div className="w-[436px] flex flex-col">
+            <h3 className="font-bold text-[40px] leading-[58px] mb-5 text-[#B28539]">{t('games_page.how_join')}</h3>
+            <span className="font-normal text-2xl leading-8 mt-8">{t('games_page.join_description')}</span>
+          </div>
+        </div>
       </div>
-
-      <p className="text-center my-auto mt-10 md:mt-auto -mb-10 md:+mb-0 md:!my-28 text-xl sm:text-2xl md:text-3xl font-medium">
-        {t('more_games')}
-      </p>
     </section>
   );
 };
