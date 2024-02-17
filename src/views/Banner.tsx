e30c33e5400e11e69f79bd54f33a98ff9777d4c7
progspace1204 <progspace1204@gmail.com>
2024-02-17 15:34:23 -0500

    resolve: conflict

--- src/views/Banner.tsx
+++ src/views/Banner.tsx
@@ -8,12 +8,12 @@ const Banner = ({ bg_path, title, content }: any) => {
     <section className="w-full min-h-screen px-8 sm:px-auto p-5 relative text-secondary flex items-start justify-center">
       <img className="w-full h-full object-cover absolute top-0 left-0" src={bg_path} />
       <div className="w-full h-auto m-auto relative z-10 flex mt-[12rem] md:mt-[20rem] flex-col-reverse md:flex-row">
-        <div className="w-full">
+        <div className="w-[60%] m-auto">
           <h2
             dangerouslySetInnerHTML={{ __html: title }}
             className="font-primary text-3xl md:text-5xl 2xl:text-[52px] 2xl:leading-[3.6rem] font-semibold md:block text-center"
           ></h2>
-          {content ?? (
+          {!!content && (
             <div className="font-primary md:text-2xl mt-12 text-center font-medium tracking-wide">
               <p>{t('our_product.description')}</p>
             </div>
--- src/views/GameIntro.tsx
+++ src/views/GameIntro.tsx
@@ -9,7 +9,7 @@ const GameIntro = () => {
   //   setIsHover(!isHover);
   // };
   return (
-    <section className="w-full h-auto pt-10 sm:pt-20 lg:pt-12 lg:pb-16 bg-[#171717] relative flex items-center font-primary text-secondary px-5">
+    <section className="w-full h-auto pt-10 sm:pt-20 lg:pt-12 lg:pb-16 relative flex items-center font-primary text-secondary px-5">
       <div className="md:w-[75%] w-fit mx-auto flex flex-col">
         <h1 className="font-bold text-2xl sm:text-2xl md:text-3xl">{t('game')}</h1>
         <div className="pt-8 grid md:grid-cols-2 grid-cols-1 gap-x-12 2 gap-y-8 justify-items-stretch">
