fad7806100690747698b1d023af2f69691082b6b
progspace1204 <progspace1204@gmail.com>
2024-02-21 10:33:38 -0800

    fix the issue in deploy

--- src/views/Games/GamesTitle.tsx
+++ src/views/Games/GamesTitle.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
