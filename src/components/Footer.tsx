933334a8bab595780fa05f245bab527083464dfa
progspace1204 <progspace1204@gmail.com>
2024-02-22 19:56:38 -0800

    fix mdemail in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 // import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
-import { MdEmail } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 // import Button3 from './Button3';
