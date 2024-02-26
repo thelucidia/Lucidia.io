d7e180e349da8a1e43a63601c99f43600ba9871b
progspace1204 <progspace1204@gmail.com>
2024-02-26 15:02:31 -0800

    fix: latest news

--- src/views/LatestNews.tsx
+++ src/views/LatestNews.tsx
@@ -1,6 +1,7 @@
 // import { FaPlayCircle } from "react-icons/fa";
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import parse from 'html-react-parser';
 
 const LatestNews = () => {
   const { t } = useTranslation();
@@ -11,17 +12,21 @@ const LatestNews = () => {
         <h1 className="font-bold text-2xl sm:text-2xl md:text-5xl">{t('latest_news')}</h1>
         <div className="mt-8 w-full grid grid-cols-1 xl:grid-cols-2 gap-8">
           <div>
-            <img src={`/free/news${index + 1}.png`} className="w-full" />
+            <img src={`/free/news${index}.png`} className="w-full" />
             <div className="mt-4">
               <div className="text-sm text-[#F0F0F0]">{t(`posted_date${index}`)}</div>
               <div className="mt-4 text-3xl font-bold text-ellipsis line-clamp-2">{t(`news_title${index}`)}</div>
-              <div className="mt-6 text-xl">{t(`news_title${index}`)}</div>
+              <div className="mt-6 text-xl">
+                {parse((t(`news_content${index}`, { returnObjects: true }) as string[]).join('<br/><br/>'))}
+              </div>
             </div>
           </div>
 
-          <div className="xl:grid-cols-3">
-            <div className="w-full md:flex gap-4 items-start mb-14">
-              <img src="/free/news2.png" className="lg:w-auto w-full" />
+          <div className="">
+            <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-4 items-start mb-14">
+              <div>
+                <img src="/free/news1.png" className="w-full" />
+              </div>
               <div className="lg:col-span-2">
                 <div className="text-sm text-[#F0F0F0]">{t('posted_date1')}</div>
                 <div className="cursor-pointer mt-4 text-xl font-bold hover:text-primary" onClick={() => setIndex(1)}>
@@ -29,8 +34,10 @@ const LatestNews = () => {
                 </div>
               </div>
             </div>
-            <div className="w-full mt-4 md:flex gap-4 items-start mb-14">
-              <img src="/free/news3.png" className="lg:w-auto w-full" />
+            <div className="w-full mt-4 grid grid-cols-1 lg:grid-cols-3 gap-4 items-start mb-14">
+              <div>
+                <img src="/free/news2.png" className="w-full" />
+              </div>
               <div className="lg:col-span-2">
                 <div className="text-sm text-[#F0F0F0]">{t('posted_date2')}</div>
                 <div className="mt-4 cursor-pointer text-xl hover:text-primary font-bold" onClick={() => setIndex(2)}>
@@ -38,12 +45,25 @@ const LatestNews = () => {
                 </div>
               </div>
             </div>
-            <div className="w-full mt-4 md:flex gap-4 items-start">
-              <img src="/free/news4.png" className="lg:w-auto w-full" />
+            <div className="w-full mt-4 grid grid-cols-1 lg:grid-cols-3 gap-4 items-start mb-14">
+              <div>
+                <img src="/free/news3.png" className="w-full" />
+              </div>
               <div className="lg:col-span-2">
                 <div className="text-sm text-[#F0F0F0]">{t('posted_date3')}</div>
                 <div className="mt-4 cursor-pointer hover:text-primary text-xl font-bold" onClick={() => setIndex(3)}>
                   {t('news_title3')}
+                </div>
+              </div>
+            </div>
+            <div className="w-full mt-4 grid grid-cols-1 lg:grid-cols-3 gap-4 items-start">
+              <div>
+                <img src="/free/news4.png" className="w-full" />
+              </div>
+              <div className="lg:col-span-2">
+                <div className="text-sm text-[#F0F0F0]">{t('posted_date4')}</div>
+                <div className="mt-4 cursor-pointer hover:text-primary text-xl font-bold" onClick={() => setIndex(4)}>
+                  {t('news_title4')}
                 </div>
               </div>
             </div>
