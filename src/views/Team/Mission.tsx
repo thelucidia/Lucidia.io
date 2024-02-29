80e015a134f563cfacca08a754801eddc933e8ea
progspace1204 <progspace1204@gmail.com>
2024-02-29 04:26:38 -0800

    fix: resposive in team page mission part

--- src/views/Team/Mission.tsx
+++ src/views/Team/Mission.tsx
@@ -10,38 +10,25 @@ const Mission = () => {
 
   return (
     <section className="w-full h-auto bg-[#1E1E1E] relative py-20 px-8 font-primary text-secondary">
-      <div className="max-w-[78.5%] w-full mx-auto flex flex-col gap-y-[92px]">
-        <div className="md:flex gap-x-[10rem]">
-          <div className="font-bold text-[52px] md:text-right ">
+      <div className="max-w-[78.5%] w-full mx-auto grid lg:grid-cols-3 sm:grid-cols-1 gap-y-[92px]">
+        <div className="flex">
+          <div className="font-bold text-[52px] text-right ">
             <div className="">{t('mission')}</div>
             <div className="">{t('statement')}</div>
           </div>
-          <div className="pl-[12rem] w-[720px] md:border-l-2 text-[24px]">{t('mission_statement')}</div>
         </div>
-        <div className="md:flex gap-x-[10rem]">
-          <div className="font-bold text-[52px] md:text-right ">
+        <div className="col-span-2 border-l-2 xl:pl-[120px] lg:pl-[90px] md:pl-[70px] sm:pl-[50px] pl-[30px]  text-[24px]">
+          {t('mission_statement')}
+        </div>
+        <div className="flex">
+          <div className="font-bold  text-[52px] text-right ">
             <div className="">{t('vision')}</div>
             <div className="">{t('future')}</div>
           </div>
-          <div className="pl-[12rem] md:border-l-2 w-[720px] text-[24px]">{t('vision_content')}</div>
         </div>
-        {/* <h1 className="sm:text-5xl text-2xl font-bold">{t("team.mission")}</h1>
-        <div className="mt-10 space-y-3 sm:space-y-8">
-          {(
-            t('team.mission_data', {
-              returnObjects: true,
-            }) as MissionInterface[]
-          ).map((items, i) => (
-            <div className="flex gap-x-5" key={i}>
-              <h1 className="text-2xl sm:text-5xl font-bold">{i + 1}.</h1>
-              <div className="text-base sm:text-2xl">
-                <p>
-                  <span className="text-primary font-bold">{items.bold}</span> {items.normal}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div> */}
+        <div className="col-span-2 border-l-2 xl:pl-[120px] lg:pl-[90px] md:pl-[70px] sm:pl-[50px] pl-[30px] text-[24px]">
+          {t('vision_content')}
+        </div>
       </div>
     </section>
   );
